a46bcd3d8fd6e00eeec87295a3cae1ca4d347018
JCPasillas <carlospasillas562@gmail.com>
2020-02-14 08:33:49 -0800

    initialized main activity components

--- app/src/main/java/com/example/assignment01/MainActivity.java
+++ app/src/main/java/com/example/assignment01/MainActivity.java
@@ -6,16 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mbtnSignUp;
-    private Button mbtnLogin;
+    private EditText loginUsername, loginPassword;
+    private Button mbtnSignUp, mbtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginUsername = findViewById(R.id.login_username);
+        loginPassword = findViewById(R.id.login_password);
 
         mbtnSignUp = findViewById(R.id.btn_signup);
         mbtnSignUp.setOnClickListener(new View.OnClickListener() {
